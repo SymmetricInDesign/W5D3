cf5014d4f506b87c2450e00417c94fb3f57b74b9
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-19 14:46:36 -0400

    finish database schema and seeding

--- aa_questions/import_db.sql
+++ aa_questions/import_db.sql
@@ -1,39 +1,43 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   fname TEXT NOT NULL,
   lname TEXT NOT NULL
-)
+);
 
-DROP TABLE IF EXISTS questions;
+
 
 CREATE TABLE questions (
-  id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
   author_id INTEGER NOT NULL,
 
   FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
 
-DROP TABLE IF EXISTS question_follows;
+
 
 CREATE TABLE question_follows (
-  id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   questions_id INTEGER NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (questions_id) REFERENCES questions(id)
-)
+);
 
-DROP TABLE IF EXISTS replies;
+
 
 CREATE TABLE replies (
-  id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY,
   author_id INTEGER NOT NULL,
   questions_id INTEGER NOT NULL,
   parent_id INTEGER,
@@ -42,18 +46,17 @@ CREATE TABLE replies (
   FOREIGN KEY (author_id) REFERENCES users(id),
   FOREIGN KEY (questions_id) REFERENCES questions(id),
   FOREIGN KEY (parent_id) REFERENCES replies(id)
-)
+);
 
-DROP TABLE IF EXISTS question_likes;
 
 CREATE TABLE question_likes (
-  id INTEGER NOT NULL,
+  id INTEGER PRIMARY KEY,
   questions_id INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (questions_id) REFERENCES questions(id)
-)
+);
 
 
 INSERT INTO
@@ -76,7 +79,22 @@ VALUES
   ((SELECT id FROM users WHERE fname = 'Sammy'), (SELECT id FROM questions WHERE title = 'ORM'));
 
 INSERT INTO
-  replies (author_id, parent_id, questions_id, body)
+  replies (author_id, questions_id, parent_id, body)
 VALUES
-  ((SELECT id FROM users WHERE fname = 'Donnie'), NULL, (SELECT id FROM questions WHERE title = 'CSS'), 'Cool!'),
-  ((SELECT id FROM users WHERE fname = 'Sammy'), N);
+  ((SELECT id FROM users WHERE fname = 'Donnie'), (SELECT id FROM questions WHERE title = 'CSS'), NULL, 'Cool!');
+
+INSERT INTO
+  replies (author_id, questions_id, parent_id, body)
+VALUES
+  ((SELECT id FROM users WHERE fname = 'Sammy'), (SELECT id from questions WHERE title = 'CSS'), (SELECT id FROM replies WHERE body = 'Cool!'), 'Dope!');
+
+INSERT INTO
+  replies (author_id, questions_id, parent_id, body)
+VALUES
+  ((SELECT id FROM users WHERE fname = 'Arthur'), (SELECT id from questions WHERE title = 'CSS'), (SELECT id FROM replies WHERE body = 'Dope!'), 'Rad!');
+
+INSERT INTO
+  question_likes (questions_id, user_id)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'ORM'), (SELECT id FROM users WHERE fname = 'Arthur')),
+  ((SELECT id FROM questions WHERE title = 'ORM'), (SELECT id FROM users WHERE fname = 'Donnie'));
